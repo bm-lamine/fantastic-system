a1b6c516f25e400fe277ac3894a76e07ca49afb1
bm-lamine <bennouioua.mohammed.lamine@gmail.com>
2025-12-15 22:29:21 +0100

    updates and preparations

--- real-auth-system/src/index.ts
+++ real-auth-system/src/index.ts
@@ -1,9 +1,13 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { env } from "~/env";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(logger());
+app.get("/", (c) => c.text("Hello Hono!"));
 
-export default app
+export default {
+  fetch: app.fetch,
+  port: env.PORT,
+};
